0e3b63b83df548e311c56067fff370100fcfac02
EDWARDWEE-um <edwardwee11@gmail.com>
2021-03-09 16:52:55 +0800

    Changed Q12

--- Lab1/Q12.py
+++ Lab1/Q12.py
@@ -1,28 +1,17 @@
-nterms = int(input("How many terms? "))
+# Python program to display the Fibonacci sequence
 
-# first two terms
-n1, n2 = 0, 1
-count = 0
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = int(input("Enter num : "))
 
 # check if the number of terms is valid
 if nterms <= 0:
-   print("Please enter a positive integer")
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(1)
+   print("Plese enter a positive integer")
 else:
    print("Fibonacci sequence:")
-   while count < nterms+1:
-       if n1 !=0:
-           print(n1)
-           nth = n1 + n2
-           # update values
-           n1 = n2
-           n2 = nth
-           count += 1
-       else:
-           nth = n1 + n2
-           # update values
-           n1 = n2
-           n2 = nth
-           count += 1
+   for i in range(nterms):
+       print(recur_fibo(i))
